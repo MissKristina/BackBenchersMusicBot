e06f18c53f66119617f6950fb8e8aeaa31d577b5
MissKristina <89345123+MissKristina@users.noreply.github.com>
2021-09-21 12:22:42 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -152,8 +152,8 @@ async def play(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="🥀ℝ𝚒ℤ𝚘𝚎𝕃",
-                        url="https://t.me/TheRiZoeL")
+                        text="• ᴍɪsᴛʏ ✘ ʙᴏᴛ • ⚡🖤",
+                        url="https://t.me/MISTYXBOT")
                    
                 ]
             ]
